af5b5e948f27220f02e90d7571a67c0f0ae18be9
MarcJoseph17 <115659452+MarcJoseph17@users.noreply.github.com>
2023-08-04 13:12:16 -0400

    2nd push

---
+++ js/responsive.js
@@ -0,0 +1,50 @@
+// collab footer form spree
+function showPopupMessage(message) {
+    console.log("Showing popup message:", message);
+  
+    const popupMessage = document.createElement("div");
+    popupMessage.className = "popup-message";
+    popupMessage.textContent = message;
+  
+    document.body.appendChild(popupMessage);
+  
+    setTimeout(() => {
+      console.log("Removing popup message");
+      popupMessage.style.display = "none";
+      document.body.removeChild(popupMessage);
+    }, 3000);
+  }
+  
+  function submitEmail() {
+    console.log("Submitting email...");
+  
+    const emailInput = document.getElementById("emailInput");
+    const formData = new FormData();
+    formData.append("email", emailInput.value);
+  
+    fetch("https://formspree.io/f/mzbldrae", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Email submitted successfully!");
+          // Show the pop-up message
+          showPopupMessage("Collaboration email submitted, I'll be in touch as soon as possible!");
+  
+          // Reset the input value after successful submission
+          emailInput.value = "";
+        } else {
+          // Handle error
+          console.log("Error submitting email");
+          alert("Oops! Something went wrong. Please try again later.");
+        }
+      })
+      .catch((error) => {
+        // Handle network errors
+        console.log("Network error occurred:", error);
+        alert("Network error occurred. Please try again later.");
+      });
+  }
+
+
---
+++ js/script.js
@@ -0,0 +1,162 @@
+$(document).ready(function () {
+    $(".animated-progress span").each(function () {
+        var progress = $(this).data("progress");
+        $(this).animate(
+            {
+                width: progress + "%",
+            },
+            1000
+        );
+        $(this).text(progress + "%");
+    });
+});
+
+//Text Script Starts here 
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Web Developer", "Open Source Contributor", "Freelancer", "AI Enthusiast", "Frontend Enthusiast", "UI/UX Designer", "Photographer", "Technical Writer", "Sports Junkie"],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Web Developer", "Open Source Contributor", "Freelancer", "AI Enthusiast", "Front End Enthusiast", "UI Designer", "Photographer", "Technical Writer", "Author", "And Sports Junkie"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+//Circle Animation Starts here 
+
+function lines(){
+  //random width size
+  let sizeW = Math.random() * 12;
+  //duration for animation
+  let duration = Math.random() * 3;
+  let color = getrandomcolor();
+  
+  
+  let div = document.createElement('div');
+  div.setAttribute('class','circle');
+  document.body.appendChild(div);
+  div.style.width = 2 + sizeW + 'px';
+  div.style.left = Math.random() * + innerWidth + 'px';
+  div.style.animationDuration = 2 + duration + 's';
+  div.style.background = color;
+  div.style.boxShadow = `0 0 10px ${color},
+             0 0 20px ${color},
+             0 0 30px ${color},
+             0 0 50px ${color}`;
+  div.style.setProperty('--background', color);
+  
+  
+
+}
+
+setInterval(function(){
+  lines();
+},200);
+
+
+
+//get random color for circles
+
+function getrandomcolor(){
+  
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  
+  for(var i = 0; i < 6; i++){
+    
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  
+  return color;
+  
+  
+}
+  
+  //Circle Animation Ends here 
+
+  function loading() {
+    document.querySelectorAll(".bar").forEach(function(current) {
+      let startWidth = 0;
+      const endWidth = current.dataset.size;
+      
+      /* 
+      setInterval() time sholud be set as trasition time / 100. 
+      In our case, 2 seconds / 100 = 20 milliseconds. 
+      */
+      const interval = setInterval(frame, 20);
+  
+      function frame() {
+        if (startWidth >= endWidth) {
+          clearInterval(interval);
+        } else {
+            startWidth++;
+            current.style.width = `${endWidth}%`;
+            current.firstElementChild.innerText = `${startWidth}%`;
+          }
+       }
+    });
+  }
+  
+  setTimeout(loading, 1000);
